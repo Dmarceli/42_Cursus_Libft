f2a317fc5a3a38f33890f1f5521a08b578b242ed
Daniel Marcelino sequeira <dmarceli@c1r5s5.42lisboa.com>
2021-10-26 17:11:49 +0100

    major issues in strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,34 +6,39 @@
 /*   By: dmarceli <dmarceli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 13:09:01 by dmarceli          #+#    #+#             */
-/*   Updated: 2021/10/26 17:00:01 by dmarceli         ###   ########.fr       */
+/*   Updated: 2021/10/26 17:08:32 by dmarceli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int			i;
-	char const	*s3;
-	int			len;
+	char	*res;
+	int		size;
 
-	if (s1 == 0)
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	res = ft_calloc(size, 1);
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return ((char *)s2);
+	if (!s2)
+		return ((char *)s1);
+	if (res)
 	{
-		
+		ft_strlcat(res, s1, size);
+		ft_strlcat(res, s2, size);
+		return (res);
 	}
+	return (0);
+}
+
+int main()
+{
+ char    *string;
+ string = ft_strjoin("ola", " tudo bem?");
+ printf("%s\n", string);
+ free(string);
+ return 0;
 }
